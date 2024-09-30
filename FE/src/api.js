1044d5edc2f6c32767d44b9f41e72444bca41336
Ritu Verma <ritu1298verma@gmail.com>
2024-09-30 15:50:00 +0530

    Update api.js

--- FE/src/api.js
+++ FE/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/athletes'; 
+const API_URL = `${import.meta.env.VITE_BASE_URL}/api/athletes`; 
 
 export const fetchAllAthletes = async () => {
   try {
